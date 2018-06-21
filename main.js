2a550e589dbf35491acc325c8b8a89a9ee178b2b
chengzichao <chengzichao@lansheng8.com>
2018-06-21 10:37:04 +0800

    完成blockchain的构造函数

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const SHA256 = REQUIRE('crypto-js/sha256');
 
 class Block {
-  constructor (timestamp, data) {
+  constructor(timestamp, data) {
     this.index = 0; // block位于chain的位置
     this.timestamp = timestamp;
     this.data = data;
@@ -17,5 +17,43 @@ class Block {
 
   mainBlock(difficulty) {
 
+  }
+}
+
+class BlockChain {
+  // 构造函数实例化blockchain
+  constructor() {
+    this.chain = [this.createGenesis];
+  }
+  // 创建chain的每一个第一个block
+  createGenesis() {
+    return new Block(0, '01/01/2017', 'Genesis block', '0')
+  }
+  // 获取最新的block
+  latestBlock() {
+    return this.chain[this.chain.length - 1];
+  }
+
+  addBlock(newBlock) {
+    newBlock.previousHash = this.latestBlock().hash;
+    newBlock.hash = newBlock.calculateHash();
+    this.chain.push(newBlock);
+  }
+
+  checkValid() {
+    for (let i = 1; i < this.chain.length; i++) {
+      const currentBlock = this.chain[i];
+      const previousBlock = this.chain[i - 1];
+
+      if (currentBlock.hash !== currentBlock.calculateHash()) {
+        return false;
+      }
+
+      if (currentBlock.previousHash !== previousBlock.hash) {
+        return false;
+      }
+    }
+
+    return true;
   }
 }
